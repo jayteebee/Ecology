41946230a46970f8ea14ed69fd8a17a8e9c8a5e5
Jayteebee <jethroblock22@gmail.com>
2024-03-18 16:43:28 +0000

    Remove commented out code in hero component

--- components/hero.tsx
+++ components/hero.tsx
@@ -27,26 +27,10 @@ export default function Hero() {
             <h1 className="h1 mb-4" data-aos="fade-up">Affordable Thermal Vision for Bat Ecology Surveys</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Elevate your ecological surveys with cutting-edge thermal imaging technology, designed to enhance bat research and conservation efforts.</p>
 
-            
-            {/* <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
-              <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-              </div>
-              <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-              </div>
-            </div> */}
+        
 
           </div>
 
-          {/* <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={1024}
-            thumbHeight={576}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} /> */}
 
         </div>
 
