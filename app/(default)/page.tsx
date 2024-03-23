224b27139eed651f9e4004e95e903027fdb59fad
Jethro_Block <jethroblock22@gmail.COM>
2024-03-23 14:15:04 +0000

    Refactor imports and fix formatting in page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,41 +1,35 @@
-
-
 export const metadata = {
-  title: 'Ecology',
-  description: 'More information about our ecology package',
-}
+  title: "Ecology",
+  description: "More information about our ecology package",
+};
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Zigzag from '@/components/zigzag'
-import ModalVideo from '@/components/modal-video'
-import Testimonials from '@/components/testimonials'
+import Hero from "@/components/hero";
+import Features from "@/components/features";
+import Zigzag from "@/components/zigzag";
+import ModalVideo from "@/components/modal-video";
+import Testimonials from "@/components/testimonials";
 
-import Maps from '../../components/Maps'
-import { useRef } from 'react'
+import Maps from "../../components/Maps";
+import { useRef } from "react";
 
 export default function Home() {
-
-
-
   return (
     <>
       <Hero />
       <div>
-              <ModalVideo
-            video="/videos/video.mp4"
-
-            />
+        <ModalVideo video="/videos/video.mp4" />
       </div>
 
-<div id='features'>
-      <Features />
-</div>
-<div id='3drender'>
-      <Zigzag />
-</div>
+      <div id="features">
+        <Features />
+      </div>
+      <div id="3drender">
+        <Zigzag />
+      </div>
       <Testimonials />
-      <div id='maps'><Maps /></div>
+      <div id="maps">
+        <Maps />
+      </div>
     </>
-  )
+  );
 }
