13bc9af37c41eb0cd6368e3f41d9f5dac11c3625
Jethro_Block <jethroblock22@gmail.COM>
2024-03-23 14:00:14 +0000

    formatting

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,21 +1,20 @@
-"use client"
+"use client";
 
-import Link from 'next/link'
-import MobileMenu from './mobile-menu'
-import TVELogo from "../../assets/TVElogo.jpg"
-import Image from 'next/image'
+import Link from "next/link";
+import MobileMenu from "./mobile-menu";
+import TVELogo from "../../assets/TVElogo.jpg";
+import Image from "next/image";
 
 export default function Header() {
-
   // In your Header component
-const scrollToMaps = (e) => {
-  e.preventDefault(); // Prevent default anchor behavior
-  const mapsSection = document.getElementById('maps');
+  const scrollToMaps = (e) => {
+    e.preventDefault(); // Prevent default anchor behavior
+    const mapsSection = document.getElementById("maps");
 
-  if (mapsSection) {
-    mapsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
-};
+    if (mapsSection) {
+      mapsSection.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
 
   return (
     <header className="absolute w-full z-30">
@@ -24,9 +23,8 @@ const scrollToMaps = (e) => {
           {/* Site branding */}
           <div className="shrink-0 mr-4">
             {/* Logo */}
-            
-              <Image src={TVELogo} width="200" height="200" alt="logo" />
-            
+
+            <Image src={TVELogo} width="200" height="200" alt="logo" />
           </div>
 
           {/* <nav className="hidden md:flex md:grow">
@@ -47,24 +45,24 @@ const scrollToMaps = (e) => {
               </li>
             </ul>
           </nav>  */}
-          
+
           <nav className="hidden md:flex md:grow">
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-              <a href="#maps" onClick={scrollToMaps} className="font-medium text-purple-600 hover:text-purple-200 px-4 py-3 flex items-center transition duration-150 ease-in-out cursor-pointer">
-  Maps
-</a>
-
+                <a
+                  href="#maps"
+                  onClick={scrollToMaps}
+                  className="font-medium text-purple-600 hover:text-purple-200 px-4 py-3 flex items-center transition duration-150 ease-in-out cursor-pointer"
+                >
+                  Maps
+                </a>
               </li>
             </ul>
           </nav>
 
           {/* <MobileMenu /> */}
-
         </div>
       </div>
     </header>
-  )
+  );
 }
-
-
