5a625141c1e89df9ef1186cbfedb0e51fb4528e5
Jayteebee <jethroblock22@gmail.com>
2024-04-03 10:19:38 +0100

    Remove commented out code and add new item to Zigzag component

--- components/Maps.tsx
+++ components/Maps.tsx
@@ -10,10 +10,10 @@ const Maps: React.FC = () => {
         <iframe title="By Train" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d39727.07729024622!2d-2.543228!3d51.514274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48719048d3523e8d%3A0x915c1ab91ecb9ff4!2sBristol%20Parkway!5e0!3m2!1sen!2suk!4v1711012653290!5m2!1sen!2suk" width="400" height="300" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="rounded-lg"></iframe>
       </div>
 
-      <div className="text-center">
+      {/* <div className="text-center">
         <h2 className="h2 mb-4">{MAP_HEADER[1].HEADER}</h2>
         <iframe title="By Plane" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1274925.2698226038!2d-2.717168!3d51.383053!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48718ea8a8ed263f%3A0xd844daddcc2849c4!2sBristol%20Airport!5e0!3m2!1sen!2suk!4v1711012743941!5m2!1sen!2suk" width="400" height="300" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="rounded-lg"></iframe>
-      </div>      
+      </div>       */}
 
       <div className="text-center">
         <h2 className="h2 mb-4">{MAP_HEADER[2].HEADER}</h2>
--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -153,6 +153,42 @@ export default function Zigzag() {
               </div>
             </div>
 
+                        {/* 4nd item */}
+                        <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width={640} height={405} alt="Features 02" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  {/* <div className="font-architects-daughter text-xl text-green-200 mb-2">{ZIGZAG_ITEM_CONTENT[1].INTRO}</div> */}
+                  <h3 className="h3 mb-3">{ZIGZAG_ITEM_CONTENT[1].TITLE}</h3>
+                  <p className="text-xl text-black-400 mb-4">{ZIGZAG_ITEM_CONTENT[1].TEXT}</p>
+                  {/* <ul className="text-lg text-black-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>{ZIGZAG_ITEM_CONTENT[1].BULLET_ONE}</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>{ZIGZAG_ITEM_CONTENT[1].BULLET_TWO}</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>{ZIGZAG_ITEM_CONTENT[1].BULLET_THREE}</span>
+                    </li>
+                  </ul> */}
+                </div>
+              </div>
+            </div>
+
           </div>
 
         </div>
