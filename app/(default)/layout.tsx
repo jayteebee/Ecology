f8593b4b1dc9f8fbb12c11f09bd7d2b48a68fc0e
Jayteebee <jethroblock22@gmail.com>
2024-04-10 14:48:39 +0100

    Update Google Tag Manager in layout components

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -8,7 +8,7 @@ import 'aos/dist/aos.css'
 import PageIllustration from '@/components/page-illustration'
 import Footer from '@/components/ui/footer'
 import { LinkedInInsightTag } from 'nextjs-linkedin-insight-tag'
-import { GoogleTagManager } from "@next/third-parties/google"
+// import { GoogleTagManager } from "@next/third-parties/google"
 
 
 
@@ -30,7 +30,7 @@ export default function DefaultLayout({
   return (
     <>
       <main className="grow">
-    <GoogleTagManager gtmId="GTM-TK53DNXC" />
+    {/* <GoogleTagManager gtmId="GTM-TK53DNXC" /> */}
 
         <PageIllustration />
 
