4fb034dd062e575b8b3f4b029ed6b52311bd951b
Jayteebee <jethroblock22@gmail.com>
2024-04-11 12:33:39 +0100

    Update LinkedIn Insight Tag in DefaultLayout component

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -35,10 +35,12 @@ export default function DefaultLayout({
         <PageIllustration />
 
         {children}
-    <LinkedInInsightTag />
+    {/* <LinkedInInsightTag /> */}
       </main>
 
       {/* <Footer /> */}
     </>
   )
 }
+
+// AQV6n0afpOY6WDWvpO8b6ekvnhrjQ8TSP1evhGctdnxUWvU9BBPQSIaTnBSyatqnvJ6KA2mcxLabEZ7mewzf1sDvdScwgPBTyI2OVCx9gObv_oEPZGuZvlVccry932AFRqOXHXRpoexGbPvS2uYfHBFnuuTV7OslEKa064mWwVl4bd2EK2CI1XvbuEqAMaqFGiSG4tNsjL8cQstGcGURS1jC8yxEeoBvRF7Mgkaxnync_wIwLCQkDBS11tFsGssNp6BeVz_KoH91KIeH-uKMfBR1B-wnvNjev_1Ng7HYZcBkBBCLtwPJb1e49fXJmO4eQ5gqxjikP_ECQmMukypxTHwEpep9tgd6JcHUsioHyPpwvIKnTUppCE77QQ_fAv-ZwzVIZnr8c-6kDMtdgIrsoDbSRAhtmOSaGC6nT88UmKXNezhRpjJS-TFhlFOHbfZnKrGud8mc1uTWN0O5BWI
