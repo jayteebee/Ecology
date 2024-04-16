de63e0b1f5bf80d6b44cba043d680f51e8b7a36b
Jethro Block <jethroblock22@gmail.com>
2024-04-16 10:19:55 +0100

    Add images and openGraph configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+import tveLogo from "./assets/TVElogo.jpg"
+
+const nextConfig = {
+  
+  images: {
+    domains: ['www.thermalvisionecology.com'],
+  },
+  openGraph: {
+    images: [
+      {
+        url: {tveLogo},
+        width: 800,
+        height: 600,
+        alt: 'Thermal Vision Ecology Logo',
+      },
+    ],
+  },
+}
+
 
 module.exports = nextConfig
