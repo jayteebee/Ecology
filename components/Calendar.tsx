1fe7dbbd32e245e72b257dd308c1e36fff23b341
Jethro Block <jethroblock22@gmail.com>
2024-04-24 15:51:59 +0100

    Add CORS headers to axios instance

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -43,6 +43,10 @@ const axiosInstance = axios.create({
   headers: {
       'Content-Type': 'application/json',
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
+      "Access-Control-Allow-Credentials": "true",
+      
   },
 });
 
